dbff7c601358ab7873e96778b44f04e0dbb8009d
CrazySpike <137111913+CrazySpike@users.noreply.github.com>
2025-10-04 18:47:28 -0400

    Delete build directory

--- build/Qt_6_9_0_for_macOS-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_s26-36GUI_FILE /Users/bwr/Qt/Widgets/s26-36GUI/build/Qt_6_9_0_for_macOS-Debug/s26-36GUI.app/Contents/MacOS/s26-36GUI)
-set(__QT_DEPLOY_TARGET_s26-36GUI_TYPE EXECUTABLE)
