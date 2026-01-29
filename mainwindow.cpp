342c022309c5c6584db7983c3e59045010952ec0
CrazySpike <137111913+CrazySpike@users.noreply.github.com>
2026-01-29 15:52:06 -0500

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,25 +2,11 @@
 #include "pages.h"
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent), stackedWidget(new QStackedWidget(this)) {
+    : QMainWindow(parent) {
 
     setWindowTitle("Oyster Farm Operations App");
-    setCentralWidget(stackedWidget);
 
-    setupPages();
-}
-
-void MainWindow::setupPages() {
-    auto *page1 = new Pages(Pages::Page1);
-    auto *page2 = new Pages(Pages::Page2);
-    auto *page3 = new Pages(Pages::Page3);
-
-    stackedWidget->addWidget(page1); // index 0
-    stackedWidget->addWidget(page2); // index 1
-    stackedWidget->addWidget(page3); // index 2
-
-    // --- Navigation connections ---
-    connect(page1, &Pages::goToPage2, this, [=]() { stackedWidget->setCurrentIndex(1); });
-
-    connect(page2, &Pages::goToPage3, this, [=]() { stackedWidget->setCurrentIndex(2); });
+    // Pages internally manages QStackedWidget
+    auto *pages = new Pages(this);
+    setCentralWidget(pages);
 }
