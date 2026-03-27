eddd292107fbca05bbbe451f3c33276a4dd140a6
CrazySpike <137111913+CrazySpike@users.noreply.github.com>
2026-03-27 08:39:43 -0400

    Update winch.cpp

--- winch.cpp
+++ winch.cpp
@@ -8,6 +8,101 @@ winch::winch(int num) {
     waterLevel = 4.52;
 }
 
+void winch::addID(const int id)
+{
+    winchID = id;
+}
+
+void winch::addConnection(const bool conn)
+{
+    connected = conn;
+}
+
+void winch::addSubmersion(const bool sub)
+{
+    cageDown = sub;
+}
+
+void winch::addCageHeight(const double height)
+{
+    cageHeight = height;
+}
+
+void winch::addWaterLevel(const double level)
+{
+    waterLevel = level;
+}
+
+void winch::addOperationsSchedule(const QStringList schedule)
+{
+    operationsSchedule = schedule;
+}
+
+void winch::addImages(const QStringList images)
+{
+    winchImages = images;
+}
+
+int winch::displayNumber() {
+    return winchNumber;
+}
+
+int winch::displayID()
+{
+    return winchID;
+}
+
+bool winch::isConnected()
+{
+    return connected;
+}
+
+QString winch::displayCageStatus() {
+    QString status = "Cage Status: ";
+
+    return (cageDown) ? status + "Down" : status + "Up";
+}
+
+double winch::displayCageHeight()
+{
+    return cageHeight;
+}
+
+double winch::displayWaterLevel()
+{
+    return waterLevel;
+}
+
+QString winch::displayOperationsSchedule()
+{
+    QString schedule = "Next Scheduled Action(s):";
+
+    for (const QString &entry : std::as_const(operationsSchedule))
+        schedule += "\n    " + entry;
+
+    return schedule;
+}
+
+QString winch::displayImages()
+{
+    QString images = "List of Recent Photos:";
+
+    for (const QString &entry : std::as_const(winchImages))
+        images += "\n" + entry;
+
+    return images;
+}
+
+#include "winch.h"
+
+winch::winch(int num) {
+    winchNumber = num;
+    winchID = 12345;
+    connected = true;
+    cageHeight = 2.13;
+    waterLevel = 4.52;
+}
+
 int winch::displayNumber() {
     return winchNumber;
 }
