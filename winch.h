b913c41bcbadb556b0af61b64a580a298bd33785
CrazySpike <137111913+CrazySpike@users.noreply.github.com>
2026-03-27 08:40:07 -0400

    Update winch.h

--- winch.h
+++ winch.h
@@ -1,17 +1,29 @@
 #ifndef WINCH_H
 #define WINCH_H
 
+#include <QStringList>
+
 class winch
 {
 public:
     winch(int num);
 
+    void addID(const int id);
+    void addConnection(const bool conn);
+    void addSubmersion(const bool sub);
+    void addCageHeight(const double height);
+    void addWaterLevel(const double level);
+    void addOperationsSchedule(const QStringList schedule);
+    void addImages(const QStringList images);
+
     int displayNumber();
     int displayID();
     bool isConnected();
-    bool isDown();
+    QString displayCageStatus();
     double displayCageHeight();
     double displayWaterLevel();
+    QString displayOperationsSchedule();
+    QString displayImages();
 
 private:
     int winchNumber = 0;
@@ -20,6 +32,8 @@ private:
     bool cageDown = false;
     double cageHeight = 0;
     double waterLevel = 0;
+    QStringList operationsSchedule;
+    QStringList winchImages;
 
 };
 
